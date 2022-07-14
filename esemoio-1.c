5afe178a5d5e1a32062d19216e7d0934321f2196
Islamkha <109146872+Islamkha@users.noreply.github.com>
2022-07-14 08:58:20 -0400

    Update esemoio-1.c

--- esemoio-1.c
+++ esemoio-1.c
@@ -2,14 +2,27 @@
 
 int main ()
 {
-	/* Dichiarazione Variabili*/
-	int numero; 
-	int incremento;
-	int decremento;
-	printf(" Inserice un numero\n");
-	scanf("%d",&numero);
-	printf(" Il numero inserito e:%d\n",numero);
-	printf(" L'incerimento del numero e:%d\n", numero+1, &incremento);
-	printf(" Il decrimento del numero e: %d\n", numero-1,&decremento);
-	return 0;
+                /* Dichiarazione dei variabili */
+
+        int numero; // numero preso da inpu da tastiera
+        int incremento;
+        int decremento;
+
+                /* chiedere l'utente di inserire il numero */
+
+        printf(" Inserice un numero\n");
+        scanf("%d",&numero);
+
+                /* Incremento e decremento del valore */
+
+        incremento = numero +1;
+        decremento = numero -1;
+
+                /*  stampa dei risoltati */
+
+        printf(" Il numero inserito e:%d\n",numero);
+        printf(" L'incerimento del numero e:%d\n", incremento);
+        printf(" Il decrimento del numero e: %d\n", decremento);
+
+        return 0;
 }
