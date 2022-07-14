4925d000fae1f5c6dd34105a75c963a51887e8c2
Islamkha <109146872+Islamkha@users.noreply.github.com>
2022-07-14 05:27:49 -0400

    Update esempio-2.c

--- esempio-2.c
+++ esempio-2.c
@@ -2,15 +2,32 @@
 
 int main ()
 {
-	int numero1, numero2;
-	float media;
-	printf(" Inserice primo numero\n");
-	scanf("%d",&numero1);
-	printf(" Inserice secondo numero\n");
-	scanf("%d",&numero2);
-	media = (numero1 + numero2)/2;
-	printf("Il medio e:%f\n", media);
+                /* Dichiarazioni Numeri Int */
+        
+        int numero1, numero2;
 
+                /* Dichiarazioni Variabili Float */
+        
+        float somma, media;
+        
+                /* Inserimento dei Numeri interi */
+        
+        printf(" Inserice primo numero\n");
+        scanf("%d",&numero1);
+        printf(" Inserice secondo numero\n");
+        scanf("%d",&numero2);
 
-	return 0;
+              /* Trasferimento della somma a Float */
+
+        somma = numero1+numero2;
+        
+                /* Calcolare la media */
+
+        media = somma/2;
+        
+                /* stampa la media */
+
+        printf("Il medio e:%f\n", media);
+
+        return 0;
 }
