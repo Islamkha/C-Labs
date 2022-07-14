770bbd6337ca31de4eeec7d5e99f4ab8870ff53f
Islamkha <109146872+Islamkha@users.noreply.github.com>
2022-07-14 05:33:13 -0400

    Update esempio-3.c

--- esempio-3.c
+++ esempio-3.c
@@ -2,9 +2,17 @@
 
 int main ()
 {
+	/* Dichiarazioni del Variabile */
+	
 	int numero;
+	
+	/* chiedere l'utente di inserire un numero */
+	
 	printf(" Inserice un numero\n");
 	scanf("%d",&numero);
+	
+	/* Controllare se il numero e positivo o negativo o zero */
+	
 	if (numero > 0)
 	{
 		
