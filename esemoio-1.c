c4bc631674b25a43b1e9b7c7eca671a23f2d8a31
Islamkha <109146872+Islamkha@users.noreply.github.com>
2022-07-14 11:05:43 +0200

    Update esemoio-1.c

--- esemoio-1.c
+++ esemoio-1.c
@@ -2,14 +2,12 @@
 
 int main ()
 {
+	/* Dichiarazione Variabili*/
 	int numero; 
 	int incremento;
 	int decremento;
 	printf(" Inserice un numero\n");
 	scanf("%d",&numero);
-//	scanf(a++,b);
-	//b=a++;
-	//c=a--;
 	printf(" Il numero inserito e:%d\n",numero);
 	printf(" L'incerimento del numero e:%d\n", numero+1, &incremento);
 	printf(" Il decrimento del numero e: %d\n", numero-1,&decremento);
